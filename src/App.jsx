ca6ee0ebc7af40cf259e263fe9b6f459f56cabd2
Aditya <adityakashyap2501@gmail.com>
2025-02-17 22:14:24 +0530

    did small change

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import BlocklyEditor from './components/BlocklyEditor'
-import './app.css'
+import './App.css'
 function App() {
   return (
     <div className='container'>
